f7ca20284d32c06157315f70895071fcd09d7242
Roman Tarassenko <tarassenko@me.com>
2022-09-30 17:25:37 +0300

    Cозданы модели Product и Сategory и зарегестрированы в admin.py.
    Создана и применина первая миграция.

--- e_store_app/admin.py
+++ e_store_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from e_store_app.models import Category, Product
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
--- e_store_app/models.py
+++ e_store_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+
 # Create your models here.
+
+DEFAULT_STATUS_ID = 1
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+    picture_link = models.TextField(null=True, blank=True, max_length=1000)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=DEFAULT_STATUS_ID)
+    stock = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return "{}. {}".format(self.pk, self.name)
+
+
+
+
+
+
+
+
+
