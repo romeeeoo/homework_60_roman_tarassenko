ec7359221350dbd2eee20e240bafb2f40e83e5e7
Roman Tarassenko <tarassenko@me.com>
2022-09-30 20:25:26 +0300

    -В base.html подключены статические стили. -В index.html добавлена ссылка на детальный просмотр в карточки товара. - Добавлен шаблон product.html c детальным отображением продукта. - Добавлены пути к детальному просмотру в urls.py. - Описано представление детального просмотра. - Добавлена сортировка в представление index_view.

--- e_store_app/urls.py
+++ e_store_app/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
 
-from e_store_app.views import index_view
+from e_store_app.views import index_view, product_view
 
 urlpatterns = [
-    path('', index_view, name='index')
+    path('', index_view, name='index'),
+    path('products/', index_view, name='products'),
+    path('products/<int:pk>/', product_view, name='product_detailed'),
 ]
--- e_store_app/views.py
+++ e_store_app/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from e_store_app.models import Product
 
 
 # Create your views here.
 def index_view(request):
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('category__name', 'name')
     context = {'products': products}
     return render(request, 'index.html', context)
+
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'product.html', context)
+
+
+
